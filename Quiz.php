92d4ba5478e8aa67f9a28267f87550d7a67e61a4
Decker Ayers <69859630+ayersdecker@users.noreply.github.com>
2022-03-23 15:35:24 -0400

    QUIZ SECTION UNDER CONSTRUCTION
    Do not use or reference quiz.php as a dependent file. Variables and Items will be adjusted in order to dynamically load different templates

---
+++ Quiz.php
@@ -0,0 +1,28 @@
+<?php
+<Aside>
+    <h1>Quiz</h1>
+    <p>What is ?</p>
+    <form action="quiz.php" method="post">
+        <input type="radio" name="answer" value="1">
+        <label>A. Blank A</label>
+        <br>
+        <input type="radio" name="answer" value="2">
+        <label>B. A Blank B</label>
+        <br>
+        <input type="radio" name="answer" value="3">
+        <label>C. A Blank C</label>
+        <br>
+        <input type="radio" name="answer" value="4">
+        <label>D. A Blank D</label>
+        <br>
+        <input type="submit" value="Submit">
+
+
+
+
+
+</Aside>
+
+
+
+?>
